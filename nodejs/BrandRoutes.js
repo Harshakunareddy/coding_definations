5ad061513f800dd03c735bd53bc88baddb9d5bf2
Harshakunareddy <kunareddyharshareddy@gmail.com>
2026-07-20 20:02:19 +0530

    my sql 10 queries

--- nodejs/BrandRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const BrandController = require("./BrandController");
-const router = express.Router();
-
-router.get("/", BrandController.getAll);
-
-module.exports = router;
